83acaeba420a95e117a6575180b734ffc455d027
Adeola Uthman <authman2@gmail.com>
2019-07-03 11:54:47 -0400

    Added tab icon, now to work on responsiveness

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -30608,7 +30608,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52609" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55061" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
