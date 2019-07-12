7df7d551f0a90abd11aa5210310a76c2ca8f5763
Adeola Uthman <authman2@gmail.com>
2019-07-12 15:20:13 -0400

    Fix history pushing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 
 import Home from './pages/Home.js';
 import About from './pages/About.js';
@@ -9,15 +10,15 @@ import FAQ from './pages/FAQ.js';
 
 import './styles/Index.less';
 
-
+createBrowserHistory();
 export default function App() {
     return <Router>
         <div className='app'>
             <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/thisyear' component={ThisYear} />
-            <Route path='/lastyear' component={LastYear} />
-            <Route path='/faq' component={FAQ} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/thisyear' component={ThisYear} />
+            <Route exact path='/lastyear' component={LastYear} />
+            <Route exact path='/faq' component={FAQ} />
         </div>
     </Router>
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,27 +7,16 @@ import '../styles/NavigationBar.less';
 
 // The navigation bar.
 export default function NavigationBar() {
-    // 1.) Setup the state.
-    const [goAbout, setGoAbout] = useState(false);
-    const [goThisYear, setGoThisYear] = useState(false);
-    const [goLastYear, setGoLastYear] = useState(false);
-    const [goFAQ, setGoFAQ] = useState(false);
-
-    // .) Return the react component.
+    // 1.) Return the react component.
     return <nav className='navigation-bar'>
         <img src={ScribbleLogo} alt='Scribble NYC Logo' onClick={() => {
             window.location.href = '/';
         }}/>
 
-        <button className='nav-button nav-button-1' onClick={() => setGoAbout(true)}>About</button>
-        <button className='nav-button nav-button-2' onClick={() => setGoLastYear(true)}>Last Year</button>
-        <button className='nav-button nav-button-3' onClick={() => setGoThisYear(true)}>This Year</button>
-        <button className='nav-button nav-button-4' onClick={() => setGoFAQ(true)}>FAQs</button>
+        <button className='nav-button nav-button-1' onClick={() => window.location.href = '/about'}>About</button>
+        <button className='nav-button nav-button-2' onClick={() => window.location.href = '/lastyear'}>Last Year</button>
+        <button className='nav-button nav-button-3' onClick={() => window.location.href = '/thisyear'}>This Year</button>
+        <button className='nav-button nav-button-4' onClick={() => window.location.href = '/faq'}>FAQs</button>
         <button className='primary-button'>Sign Up!</button>
-
-        { goAbout && <Redirect to='/about'/> }
-        { goLastYear && <Redirect to='/lastyear'/> }
-        { goThisYear && <Redirect to='/thisyear'/> }
-        { goFAQ && <Redirect to='/faq'/> }
     </nav>
 }
