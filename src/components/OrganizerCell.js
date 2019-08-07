3fe5312eb6ae26aa5b907b909ca6e4ee639531ba
Adeola Uthman <authman2@gmail.com>
2019-08-07 11:25:43 -0400

    Fix the face images

--- src/components/OrganizerCell.js
+++ src/components/OrganizerCell.js
@@ -7,11 +7,7 @@ export default function OrganizerCell(props) {
     const { image, name, title } = props;
 
     return <div className='organizer-cell'>
-        <img src={image} style={{
-            transform: name === 'Adeola Uthman' 
-                && window.matchMedia('(max-width: 768px)').matches === false ? 
-                    'rotateZ(90deg)' : 'none'
-        }}/>
+        <img src={image}/>
         <h3>{name}</h3>
         <p>{title}</p>
     </div>
